be653acaba3f45196989bacf7ab04234bcac70c2
Anthony <anthony.sforzin@gmail.com>
2025-05-31 18:57:31 -0300

    feat: add Hello World example for C++ basics

--- week-01_basics/lesson-01_hello_world.cpp
+++ week-01_basics/lesson-01_hello_world.cpp
@@ -1,1 +1,11 @@
 // lesson-01_hello_world.cpp
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Thats my first C++ program!" << std::endl;
+    return 0;
+
+}
+
