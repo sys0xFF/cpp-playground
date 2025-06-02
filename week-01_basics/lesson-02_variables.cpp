5c9d585370c21a13d9fa21794745fe8911633c9b
Anthony <anthony.sforzin@gmail.com>
2025-06-01 23:57:41 -0300

    feat: add examples of basic variable types and value updates

--- week-01_basics/lesson-02_variables.cpp
+++ week-01_basics/lesson-02_variables.cpp
@@ -1,1 +1,27 @@
 // lesson-02_variables.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+    int age = 18;                 
+    float height = 1.75f;           
+    double balance = 1234.56;       
+    char letter = 'A';              
+    bool alive = true;               
+
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Balance: " << balance << endl;
+    cout << "Letter: " << letter << endl;
+    cout << "Alive: " << (alive ? "Yes" : "No") << endl;
+
+    age = age + 2;
+    balance -= 200.0;
+
+    cout << "\n--- Updated ---" << endl;
+    cout << "New Age: " << age << endl;
+    cout << "New Balance: " << balance << endl;
+
+    return 0;
+}
